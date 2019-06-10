be2d2c076532a0e85aed6b06b8a9f7c5a2aeaa66
wesley_nunes <wesleysilva.ti@gmail.com>
2019-06-10 19:22:48 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+
+gulp.task('js', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        './node_modules/jquery/dist/jquery.min.js',        
+        './node_modules/jquery-validation/dist/jquery.validate.min.js',
+        './node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js',
+        './node_modules/glyphicons/glyphicons.js',
+        './node_modules/qrcode.js/src/qrcode.js',  
+        './Assets/js/site.js',
+        './Assets/js/fontawesome.min.js',
+        './Assets/js/all.min.js',
+    ])
+        //.pipe(browserSync.stream())
+        .pipe(gulp.dest('wwwroot/js/'));
+});
+
+gulp.task('css', function () {
+    return gulp.src([        
+        './Assets/css/site.css',
+        './Assets/css/fontawesome.min.css',
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',  
+        //painel admin
+        './Assets/admin/css/simple-sidebar.css',        
+    ])
+        .pipe(cssmin())    
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('images', function () {
+    return gulp.src([
+        './Assets/images/favicon.ico',      
+    ])
+        //.pipe(browserSync.stream())        
+        .pipe(gulp.dest('wwwroot/'));
+});
